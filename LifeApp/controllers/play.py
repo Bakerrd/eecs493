c1dbf59beec52c0a52d8f2217b038d26c01fe2c9
Bakerrd <bakerrd@umich.edu>
2015-04-10 16:49:45 -0400

    plays the game. it prints it. it looks beautiful.

--- LifeApp/controllers/play.py
+++ LifeApp/controllers/play.py
@@ -13,6 +13,7 @@ play = Blueprint('play', __name__, template_folder='templates')
 @play.route('/play')
 def play_route():
 
+	message = ""
 	players = []
 	for i in range(0,4):
 		player = AIPlayer(str(i))
@@ -22,50 +23,50 @@ def play_route():
 
 	while end_of_game != 4:
 		for p in players:
-			print ""
-			print "starting " + p.name + "'s turn"
+			message = message + "\n"
+			message = message + "starting " + p.name + "'s turn\n" 
 			if p.done == False:
 				cur_position = p.position
 				roll = random.randint(1, 10)
-				print "player starting from position " + str(cur_position) + " and rolled " + str(roll)
+				message = message + "player starting from position " + str(cur_position) + " and rolled " + str(roll) + "\n"
 
 				while roll > 0:
-					print "space " + str(cur_position)
+					message = message + "space " + str(cur_position) + "\n"
 					if cur_position == -1:
 						college = random.randint(0,1)
 						if college == 1:
-							print "player is going to college"
+							message = message + "player is going to college\n"
 							cur_position = 0
 						else: 
-							print "player starting career"
+							message = message +  "player starting career\n"
 							cur_position = 11
 					elif cur_position == 10:
-						print "end of college fork"
+						message = message + "end of college fork\n"
 						cur_position = 13
 					elif cur_position == 36:
 						family = random.randint(0,1)
 						if family == 1:
-							print "player takes family route"
+							message = message +  "player takes family route\n"
 							cur_position = 37
 						else:
-							print "player doesn't take family route"
+							message = message + "player doesn't take family route\n"
 							cur_position = 46
 					elif cur_position == 45:
-						print "end of family fork"
+						message = message +  "end of family fork\n"
 						cur_position = 50
 					elif cur_position == 73:
 						risky = random.randint(0,1)
 						if risky == 1:
-							print "player takes risky route"
+							message = message + "player takes risky route\n"
 							cur_position = 74
 						else:
-							print "player takes safe route"
+							message = message + "player takes safe route\n"
 							cur_position = 78
 					elif cur_position == 77:
-						print "end of risky fork"
+						message = message + "end of risky fork\n"
 						cur_position = 82
 					elif cur_position == 89:
-						print "player " + p.name + " has retired"
+						message =  message + "player " + p.name + " has retired\n"
 						p.done = True
 						roll = 0
 						end_of_game = end_of_game + 1
@@ -77,13 +78,16 @@ def play_route():
 					if cur_position in board.paydays:
 						# need to have salary
 						p.bankroll = p.bankroll + 10000 
-						print "player " + p.name + " got paid and now has " + str(p.bankroll) + " dollars"
+						message = message + "player " + p.name + " got paid and now has " + str(p.bankroll) + " dollars\n"
 
 				p.position = cur_position
-				print "player ends turn on space " + str(p.position)
+				message = message + "player ends turn on space " + str(p.position) + "\n"
 
 			else:
-				print "player is already retired"
+				message = message + "player is already retired\n"
+
+
+	return render_template('create_game.html',message=message)
 
 
 
