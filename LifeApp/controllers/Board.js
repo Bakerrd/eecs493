0a734e35f7e8e347aef46024e76fb6a180c69595
cmbeyers <cmbeyers@umich.edu>
2015-04-14 17:53:50 -0400

    Update Board.js

--- LifeApp/controllers/Board.js
+++ LifeApp/controllers/Board.js
@@ -3,7 +3,7 @@ function Board(){
 	this.paydays = [12, 21, 31, 40, 48, 54, 64, 70, 76, 79];
 	this.special = [4,7,12,18,21,25,26,28,31,34,37,38,39,40,42,43,44,45,48,52,54,56,60,64,66,70,76,79];
 		for(int i = 0; i < 90; i++){
-			var title = "tile " + str(i);
+			var title = "tile " + i.toString();
 			var new_tile = Tile(title, 10000);
 			this.tiles.push(new_tile);
 		}
