d552bf194340b0c748c4d7e7cc081538b21dd639
Bakerrd <bakerrd@umich.edu>
2015-04-16 04:12:47 -0400

    more things

--- LifeApp/db/load_data.sql
+++ LifeApp/db/load_data.sql
@@ -83,272 +83,272 @@ VALUES (9, "Drug Warehouse", 600000, 450000, "/static/images/houses/warehouse_ic
 
 #insert tiles
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (0,"Hit by a Blue Bus", 25000, 0, 0);
+VALUES (0,"Hit by a Blue Bus", 25000, 1525, 491);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (1, "Your Fraternity Wrecked A Ski Resort", -400000, 1,1);
+VALUES (1, "Your Fraternity Wrecked A Ski Resort", -400000, 1417,494);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (2, "App Got Funded", 50000, 2, 2);
+VALUES (2, "App Got Funded", 50000, 1324, 530);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (3, "Financial Aid Reimbursement", 10000, 3,3);
+VALUES (3, "Financial Aid Reimbursement", 10000, 1255,599);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (4,"Broken Condom...", 0, 4,4);
+VALUES (4,"Broken Condom...", 0, 1216,698);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (5,"Freshman Swipesy_valou Into Dining Hall", 300000, 5,5);
+VALUES (5,"Freshman Swipesy_valou Into Dining Hall", 300000, 1219,797);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (6,"Sell Previous Exam Answers", 100000, 6,6);
+VALUES (6,"Sell Previous Exam Answers", 100000, 1252,887);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (7,"Expelled for 18th Honor Code Violation", 0, 7,7);
+VALUES (7,"Expelled for 18th Honor Code Violation", 0, 1324,959);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (8, "Bought Textbooks", -200000, 8, 8);
+VALUES (8, "Bought Textbooks", -200000, 1417, 998);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (9, "Studied Abroad", -20000, 9, 9);
+VALUES (9, "Studied Abroad", -20000, 1501, 1004);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (10, "Got Fake I.D. Taken Away at Skeeps", -5000, 10, 10);
+VALUES (10, "Got Fake I.D. Taken Away at Skeeps", -5000, 1645, 938);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (11, "Car Accident", -10000, 11, 11);
+VALUES (11, "Car Accident", -10000, 1726, 695);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (12, "Payday!", 0, 12, 12);
+VALUES (12, "Payday!", 0, 1726, 782);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (13, "Ran With the Bulls in Pamplona", 50000, 13, 13);
+VALUES (13, "Ran With the Bulls in Pamplona", 50000, 1735, 896);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (14, "Sold Back Textbooks", 24, 14, 14);
+VALUES (14, "Sold Back Textbooks", 24, 1834, 965);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (15, "Became the Batman", 300000, 10, 10);
+VALUES (15, "Became the Batman", 300000, 1840, 1073);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (16, "Learn To Skydive", -50000, 10, 10);
+VALUES (16, "Learn To Skydive", -50000, 1825, 1172);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (17, "Bought a Bouncy Castle", -10000, 10, 10);
+VALUES (17, "Bought a Bouncy Castle", -10000, 1843, 1274);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (18, "Ambushed by Loan Sharks", 0, 10, 10);
+VALUES (18, "Ambushed by Loan Sharks", 0, 1930, 1325);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (19, "Won Trivia Night", 20000, 10, 10);
+VALUES (19, "Won Trivia Night", 20000, 2011, 1409);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (20, "Started a Snail Farm", -10000, 10, 10);
+VALUES (20, "Started a Snail Farm", -10000, 2026, 1526);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (21, "Payday!", 0, 10, 10);
+VALUES (21, "Payday!", 0, 1987, 1646);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (22, "Successfully Laundered Money", 150000, 10, 10);
+VALUES (22, "Successfully Laundered Money", 150000, 1903, 1718);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (23, "Started a Recorder Band", -25000, 10, 10);
+VALUES (23, "Started a Recorder Band", -25000, 1789, 1739);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (24, "Took Online Survey", 5000, 10, 10);
+VALUES (24, "Took Online Survey", 5000, 1681, 1679);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (25, "Got Married to a Liberal Arts Major", -25000, 10, 10);
+VALUES (25, "Got Married to a Liberal Arts Major", -25000, 1609, 1595);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (26, "Broken Condom...", 0, 10, 10);
+VALUES (26, "Broken Condom...", 0, 1507, 1565);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (27, "Developed an Addiction to Meth", -200000, 10, 10);
+VALUES (27, "Developed an Addiction to Meth", -200000, 1384, 1610);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (28, "Divorce", 0, 10, 10);
+VALUES (28, "Divorce", 0, 1270, 1652);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (29, "In-laws Moved In", -100000, 10, 10);
+VALUES (29, "In-laws Moved In", -100000, 1150, 1700);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (30, "Tripped Down an Escalator at the Mall", 100000, 10, 10);
+VALUES (30, "Tripped Down an Escalator at the Mall", 100000, 1022, 1745);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (31, "Payday!", 0, 10, 10);
+VALUES (31, "Payday!", 0, 896, 1766);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (32, "Invest in a New Resturant", 50000, 10, 10);
+VALUES (32, "Invest in a New Resturant", 50000, 803, 1733);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (33, "Became a Heroin Hero", 40000, 10, 10);
+VALUES (33, "Became a Heroin Hero", 40000, 770, 1637);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (34, "Ambushed by Loan Sharks", 0, 10, 10);
+VALUES (34, "Ambushed by Loan Sharks", 0, 764, 1532);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (35, "Identity Was Stolen", -20000, 10, 10);
+VALUES (35, "Identity Was Stolen", -20000, 722, 1427);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (36, "Losty_valour Keys", -5000, 10, 10);
+VALUES (36, "Losty_valour Keys", -5000, 632, 1364);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (37, "Second Wedding Ceremony", -25000, 10, 10);
+VALUES (37, "Second Wedding Ceremony", -25000, 449, 1295);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (38, "Had a Baby!", 0, 10, 10);
+VALUES (38, "Had a Baby!", 0, 341, 1256);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (39, "Had Twins!", 0, 10, 10);
+VALUES (39, "Had Twins!", 0, 242, 1208);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (40, "Payday!", 0, 10, 10);
+VALUES (40, "Payday!", 0, 185, 1112);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (41, "Won a Lifetime Achievement Award", 100000, 10, 10);
+VALUES (41, "Won a Lifetime Achievement Award", 100000, 209, 1016);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (42, "Had Triplets!", 0, 10, 10);
+VALUES (42, "Had Triplets!", 0, 281, 926);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (43, "Had a Baby!", 0, 10, 10);
+VALUES (43, "Had a Baby!", 0, 380, 854);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (44, "Had Twins!", 0, 10, 10);
+VALUES (44, "Had Twins!", 0, 494, 827);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (45, "Broken Condom...", 0, 10, 10);
+VALUES (45, "Broken Condom...", 0, 611, 857);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (46, "Hooked Up With Batman", 50000, 10, 10);
+VALUES (46, "Hooked Up With Batman", 50000, 623, 1256);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (47, "Sold Soul", 40000, 10, 10);
+VALUES (47, "Sold Soul", 40000, 674, 1169);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (48, "Payday!", 0, 10, 10);
+VALUES (48, "Payday!", 0, 623, 1064);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (49, "Found Daughter Working at a Strip Club", -25000, 0,0);
+VALUES (49, "Found Daughter Working at a Strip Club", -25000, 671,983);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (50, "Formed A Family Band", -10000, 0,0);
+VALUES (50, "Formed A Family Band", -10000, 716,920);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (51, "Got Diabeetus", -200000, 10, 10);
+VALUES (51, "Got Diabeetus", -200000, 827, 986);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (52, "Got Caught Cheating... Divorce", 0, 10, 10);
+VALUES (52, "Got Caught Cheating... Divorce", 0, 920, 1058);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (53, "Recorded Song on Recorder, Sold it to Kanye", 100000, 10, 10);
+VALUES (53, "Recorded Song on Recorder, Sold it to Kanye", 100000, 1010, 1124);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (54, "Payday!", 0, 10, 10);
+VALUES (54, "Payday!", 0, 1099, 1190);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (55, "Got Caught Pirating Game of Thrones", -50000, 10, 10);
+VALUES (55, "Got Caught Pirating Game of Thrones", -50000, 1189, 1259);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (56, "Third Wedding Ceremony...", -25000, 10, 10);
+VALUES (56, "Third Wedding Ceremony...", -25000, 1288, 1344);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (57, "Flat Tire", -5000, 10, 10);
+VALUES (57, "Flat Tire", -5000, 1384, 1391);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (58, "Collected 30 Bitcoins", 40000, 10, 10);
+VALUES (58, "Collected 30 Bitcoins", 40000, 1507, 1430);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (59, "Stopped Robots Taking Over the World", 80000, 10, 10);
+VALUES (59, "Stopped Robots Taking Over the World", 80000, 1501, 1322);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (60, "Mauled by a Chipmunk.", -150000, 10, 10);
+VALUES (60, "Divorce", 0, 1438, 1235);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (61, "Got Bit by a Dog. Called Sam", 500000, 10, 10);
+VALUES (61, "Got Bit by a Dog. Called Sam", 500000, 1360, 1151);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (62, "Got a Speeding Ticket on a Moped", -80000, 10, 10);
+VALUES (62, "Got a Speeding Ticket on a Moped", -80000, 1273, 1067);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (63, "Got a D.U.I. Riding a Bike", -50000, 10, 10);
+VALUES (63, "Got a D.U.I. Riding a Bike", -50000, 1183, 986);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (64, "Payday!", 0, 10, 10);
+VALUES (64, "Payday!", 0, 1096, 908);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (65, "Won the Lottery", 200000, 10, 10);
+VALUES (65, "Won the Lottery", 200000, 1004, 839);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (66, "Ambushed by Loan Sharks!", 0, 10, 10);
+VALUES (66, "Ambushed by Loan Sharks!", 0, 917, 764);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (67, "Won ESPN Bracket Challenge", 10000, 10, 10);
+VALUES (67, "Won ESPN Bracket Challenge", 10000, 815, 698);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (68, "Sold Futon on Craigslist", 15000, 10, 10);
+VALUES (68, "Sold Futon on Craigslist", 15000, 713, 638);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (69, "Donated Plamsa", 30000, 10, 10);
+VALUES (69, "Donated Plamsa", 30000, 611, 593);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (70, "Payday!", 0, 10, 10);
+VALUES (70, "Payday!", 0, 488, 566);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (71, "Charged With Indecent Exposure", -150000, 10, 10);
+VALUES (71, "Charged With Indecent Exposure", -150000, 368, 551);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (72, "Contracted Smallpox... Somehow", -100000, 10, 10);
+VALUES (72, "Contracted Smallpox... Somehow", -100000, 263, 521);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (73, "Pillaged a Neighboring Town", 75000, 10, 10);
+VALUES (73, "Pillaged a Neighboring Town", 75000, 218, 449);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (74, "Found Waldo", 20000, 10, 10);
+VALUES (74, "Found Waldo", 20000, 227, 260);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (75, "Got an Article Published", 10000, 10, 10);
+VALUES (75, "Got an Article Published", 10000, 275, 167);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (76, "Payday!", 0, 10, 10);
+VALUES (76, "Payday!", 0, 347, 98);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (77, "Lost Money in a Pyramid Scheme", -50000, 10, 10);
+VALUES (77, "Lost Money in a Pyramid Scheme", -50000, 464, 68);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (78, "Public Urination", -90000, 10, 10);
+VALUES (78, "Public Urination", -90000, 320, 362);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (79, "Payday!", 0, 10, 10);
+VALUES (79, "Payday!", 0, 434, 320);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (80, "Invest in Nuclear Fusion", -800000, 10, 10);
+VALUES (80, "Invest in Nuclear Fusion", -800000, 518, 245);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (81, "Invented in Emu-Drawn Carriages", 600000, 10, 10);
+VALUES (81, "Invented in Emu-Drawn Carriages", 600000, 554, 152);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (82, "Start a Water Farm on Mars", 500000, 10, 10);
+VALUES (82, "Start a Water Farm on Mars", 500000, 587, 65);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (83, "Built the Worlds Largest Hamster Wheel", -25000, 10, 10);
+VALUES (83, "Built the Worlds Largest Hamster Wheel", -25000, 701, 71);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (84, "Ally_valour Money Stayed in Vegas", -100000, 10, 10);
+VALUES (84, "Ally_valour Money Stayed in Vegas", -100000, 830, 89);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (85, "Probed by Aliens, Collected a Space Rock", 10000, 10, 10);
+VALUES (85, "Probed by Aliens, Collected a Space Rock", 10000, 926, 152);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (86, "Won Big on Penny Slots", 30000, 10, 10);
+VALUES (86, "Won Big on Penny Slots", 30000, 983, 257);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (87, "Goty_valour Hands a Little Dirty", 50000, 10, 10);
+VALUES (87, "Goty_valour Hands a Little Dirty", 50000, 995, 377);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (88, "Talked About Fight Club", -100000, 10, 10);
+VALUES (88, "Talked About Fight Club", -100000, 929, 467);
 
 INSERT INTO Tiles (tile_id, title, value, x_val,y_val)
-VALUES (89, "Found Life on the Moon", 50, 10, 10);
+VALUES (89, "Found Life on the Moon", 50, 809, 455);
 
